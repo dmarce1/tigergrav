a0e9dec19d69e2d10582c3f3f63c677824b62c3d
Dominic Marcello <dmarce504@gmail.com>
2020-07-08 14:09:01 -0500

    Preparing ewald

--- tigergrav/gravity.hpp
+++ tigergrav/gravity.hpp
@@ -26,7 +26,7 @@ std::uint64_t gravity_mono_mono(std::vector<force> &g, const std::vector<vect<po
 std::uint64_t gravity_mono_multi(std::vector<force> &g, const std::vector<vect<pos_type>> &x, std::vector<multi_source> &y, const bool do_phi);
 std::uint64_t gravity_multi_multi(expansion<double> &, const vect<float> &x, std::vector<multi_source> &y);
 std::uint64_t gravity_multi_mono(expansion<double> &, const vect<float> &x, std::vector<vect<pos_type>> &y);
-std::uint64_t gravity_ewald(std::vector<force> &g, const std::vector<vect<float>> &x, std::vector<mono_source> &y, const bool do_phi);
+std::uint64_t gravity_ewald(std::vector<force> &g, const std::vector<vect<pos_type>> &x, std::vector<mono_source> &y, const bool do_phi);
 float ewald_near_separation(const vect<float> x);
 float ewald_far_separation(const vect<float> x);
 void init_ewald();
