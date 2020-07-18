2e2b212460f952f079b83a701cf8e6fb9244b1aa
Dominic Marcello <dmarce504@gmail.com>
2020-07-18 06:27:23 -0500

    added avx512

--- tigergrav/avx_mathfun.h
+++ tigergrav/avx_mathfun.h
@@ -40,13 +40,6 @@ typedef __m256  v8sf; // vector of 8 float (avx)
 typedef __m256i v8si; // vector of 8 int   (avx)
 typedef __m128i v4si; // vector of 8 int   (avx)
 
-#define _PI32AVX_CONST(Name, Val)                                            \
-  static const ALIGN32_BEG int _pi32avx_##Name[4] ALIGN32_END = { Val, Val, Val, Val }
-
-_PI32AVX_CONST(1, 1);
-_PI32AVX_CONST(inv1, ~1);
-_PI32AVX_CONST(2, 2);
-_PI32AVX_CONST(4, 4);
 
 
 /* declare some AVX constants -- why can't I figure a better way to do that? */
@@ -272,4 +265,242 @@ inline void sincos256_ps(v8sf x, v8sf *s, v8sf *c) {
   *c = _mm256_xor_ps(xmm2, sign_bit_cos);
 }
 
+#ifdef __AVX512F__
 
+
+/* __m128 is ugly to write */
+typedef __m512  v16sf; // vector of 8 float (avx)
+typedef __m512i v16si; // vector of 8 int   (avx)
+
+
+
+/* declare some AVX constants -- why can't I figure a better way to do that? */
+#define _PS512_CONST(Name, Val)                                            \
+  static const ALIGN32_BEG float _ps512_##Name[16] ALIGN32_END = { Val, Val, Val, Val, Val, Val, Val, Val,Val, Val, Val, Val, Val, Val, Val, Val, }
+#define _PI32_CONST512(Name, Val)                                            \
+  static const ALIGN32_BEG int _pi32_512_##Name[16] ALIGN32_END = { Val, Val, Val, Val, Val, Val, Val, Val,Val, Val, Val, Val, Val, Val, Val, Val, }
+#define _PU32_CONST512(Name, Val)                                            \
+  static const ALIGN32_BEG unsigned _pu32_512_##Name[16] ALIGN32_END = { Val, Val, Val, Val, Val, Val, Val, Val,Val, Val, Val, Val, Val, Val, Val, Val, }
+#define _PS512_CONST_TYPE(Name, Type, Val)                                 \
+  static const ALIGN32_BEG Type _ps512_##Name[16] ALIGN32_END = { Val, Val, Val, Val, Val, Val, Val, Val,Val, Val, Val, Val, Val, Val, Val, Val, }
+
+_PS512_CONST(1  , 1.0f);
+_PS512_CONST(0p5, 0.5f);
+/* the smallest non denormalized float number */
+_PS512_CONST_TYPE(min_norm_pos, int, 0x00800000);
+_PS512_CONST_TYPE(mant_mask, int, 0x7f800000);
+_PS512_CONST_TYPE(inv_mant_mask, int, ~0x7f800000);
+
+_PS512_CONST_TYPE(sign_mask, unsigned, 0x80000000);
+_PS512_CONST_TYPE(inv_sign_mask, int, ~0x80000000);
+
+_PI32_CONST512(0, 0);
+_PI32_CONST512(1, 1);
+_PI32_CONST512(inv1, ~1);
+_PI32_CONST512(2, 2);
+_PI32_CONST512(4, 4);
+_PI32_CONST512(0x7f, 0x7f);
+
+_PU32_CONST512(0xffffffff, 0xffffffff);
+
+_PS512_CONST(cephes_SQRTHF, 0.707106781186547524);
+_PS512_CONST(cephes_log_p0, 7.0376836292E-2);
+_PS512_CONST(cephes_log_p1, - 1.1514610310E-1);
+_PS512_CONST(cephes_log_p2, 1.1676998740E-1);
+_PS512_CONST(cephes_log_p3, - 1.2420140846E-1);
+_PS512_CONST(cephes_log_p4, + 1.4249322787E-1);
+_PS512_CONST(cephes_log_p5, - 1.6668057665E-1);
+_PS512_CONST(cephes_log_p6, + 2.0000714765E-1);
+_PS512_CONST(cephes_log_p7, - 2.4999993993E-1);
+_PS512_CONST(cephes_log_p8, + 3.3333331174E-1);
+_PS512_CONST(cephes_log_q1, -2.12194440e-4);
+_PS512_CONST(cephes_log_q2, 0.693359375);
+
+
+
+
+_PS512_CONST(exp_hi,	88.3762626647949f);
+_PS512_CONST(exp_lo,	-88.3762626647949f);
+
+_PS512_CONST(cephes_LOG2EF, 1.44269504088896341);
+_PS512_CONST(cephes_exp_C1, 0.693359375);
+_PS512_CONST(cephes_exp_C2, -2.12194440e-4);
+
+_PS512_CONST(cephes_exp_p0, 1.9875691500E-4);
+_PS512_CONST(cephes_exp_p1, 1.3981999507E-3);
+_PS512_CONST(cephes_exp_p2, 8.3334519073E-3);
+_PS512_CONST(cephes_exp_p3, 4.1665795894E-2);
+_PS512_CONST(cephes_exp_p4, 1.6666665459E-1);
+_PS512_CONST(cephes_exp_p5, 5.0000001201E-1);
+
+inline v16sf exp512_ps(v16sf x) {
+  v16sf tmp = _mm512_setzero_ps(), fx;
+  v16si imm0;
+  v16sf one = *(v16sf*)_ps512_1;
+
+  x = _mm512_min_ps(x, *(v16sf*)_ps512_exp_hi);
+  x = _mm512_max_ps(x, *(v16sf*)_ps512_exp_lo);
+
+  /* express exp(x) as exp(g + n*log(2)) */
+  fx = _mm512_mul_ps(x, *(v16sf*)_ps512_cephes_LOG2EF);
+  fx = _mm512_add_ps(fx, *(v16sf*)_ps512_0p5);
+
+  /* how to perform a floorf with SSE: just below */
+  //imm0 = _mm512_cvttps_epi32(fx);
+  //tmp  = _mm512_cvtepi32_ps(imm0);
+
+  tmp = _mm512_floor_ps(fx);
+
+  /* if greater, substract 1 */
+  //v16sf mask = _mm512_cmpgt_ps(tmp, fx);
+  v16sf mask = _mm512_mask_mov_ps(_mm512_setzero_ps(),_mm512_cmp_ps_mask(tmp, fx, _CMP_GT_OS),one);
+ // mask = _mm512_and_ps(mask, one);
+  fx = _mm512_sub_ps(tmp, mask);
+
+  tmp = _mm512_mul_ps(fx, *(v16sf*)_ps512_cephes_exp_C1);
+  v16sf z = _mm512_mul_ps(fx, *(v16sf*)_ps512_cephes_exp_C2);
+  x = _mm512_sub_ps(x, tmp);
+  x = _mm512_sub_ps(x, z);
+
+  z = _mm512_mul_ps(x,x);
+
+  v16sf y = *(v16sf*)_ps512_cephes_exp_p0;
+  y = _mm512_mul_ps(y, x);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_cephes_exp_p1);
+  y = _mm512_mul_ps(y, x);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_cephes_exp_p2);
+  y = _mm512_mul_ps(y, x);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_cephes_exp_p3);
+  y = _mm512_mul_ps(y, x);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_cephes_exp_p4);
+  y = _mm512_mul_ps(y, x);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_cephes_exp_p5);
+  y = _mm512_mul_ps(y, z);
+  y = _mm512_add_ps(y, x);
+  y = _mm512_add_ps(y, one);
+
+  /* build 2^n */
+  imm0 = _mm512_cvttps_epi32(fx);
+  // another two AVX2 instructions
+  imm0 = _mm512_add_epi32(imm0, *(v16si*)_pi32_512_0x7f);
+  imm0 = _mm512_slli_epi32(imm0, 23);
+  v16sf pow2n = _mm512_castsi512_ps(imm0);
+  y = _mm512_mul_ps(y, pow2n);
+  return y;
+}
+
+
+_PS512_CONST(minus_cephes_DP1, -0.78515625);
+_PS512_CONST(minus_cephes_DP2, -2.4187564849853515625e-4);
+_PS512_CONST(minus_cephes_DP3, -3.77489497744594108e-8);
+_PS512_CONST(sincof_p0, -1.9515295891E-4);
+_PS512_CONST(sincof_p1,  8.3321608736E-3);
+_PS512_CONST(sincof_p2, -1.6666654611E-1);
+_PS512_CONST(coscof_p0,  2.443315711809948E-005);
+_PS512_CONST(coscof_p1, -1.388731625493765E-003);
+_PS512_CONST(coscof_p2,  4.166664568298827E-002);
+_PS512_CONST(cephes_FOPI, 1.27323954473516); // 4 / M_PI
+
+/* since sin512_ps and cos512_ps are almost identical, sincos512_ps could replace both of them..
+   it is almost as fast, and gives you a free cosine with your sine */
+inline void sincos512_ps(v16sf x, v16sf *s, v16sf *c) {
+
+  v16sf xmm1, xmm2, xmm3 = _mm512_setzero_ps(), sign_bit_sin, y;
+  v16si imm0, imm2, imm4;
+
+
+  sign_bit_sin = x;
+  /* take the absolute value */
+  x = _mm512_and_ps(x, *(v16sf*)_ps512_inv_sign_mask);
+  /* extract the sign bit (upper one) */
+  sign_bit_sin = _mm512_and_ps(sign_bit_sin, *(v16sf*)_ps512_sign_mask);
+
+  /* scale by 4/Pi */
+  y = _mm512_mul_ps(x, *(v16sf*)_ps512_cephes_FOPI);
+
+  /* store the integer part of y in imm2 */
+  imm2 = _mm512_cvttps_epi32(y);
+
+  /* j=(j+1) & (~1) (see the cephes sources) */
+  imm2 = _mm512_add_epi32(imm2, *(v16si*)_pi32_512_1);
+  imm2 = _mm512_and_si512(imm2, *(v16si*)_pi32_512_inv1);
+
+  y = _mm512_cvtepi32_ps(imm2);
+  imm4 = imm2;
+
+  /* get the swap sign flag for the sine */
+  imm0 = _mm512_and_si512(imm2, *(v16si*)_pi32_512_4);
+  imm0 = _mm512_slli_epi32(imm0, 29);
+  //v16sf swap_sign_bit_sin = _mm512_castsi512_ps(imm0);
+
+  /* get the polynom selection mask for the sine*/
+  imm2 = _mm512_and_si512(imm2, *(v16si*)_pi32_512_2);
+  imm2 = _mm512_mask_mov_epi16(*(v16si*)_pi32_512_1, _mm512_cmpeq_epi32_mask(imm2, *(v16si*)_pi32_512_0),  *(v16si*)_pu32_512_0xffffffff);
+  //v16sf poly_mask = _mm512_castsi512_ps(imm2);
+printf( "%x %x %x %x %x %x %x %x\n", imm2[0], imm2[1], imm2[2], imm2[3], imm2[4], imm2[5], imm2[6], imm2[7]);
+  v16sf swap_sign_bit_sin = _mm512_castsi512_ps(imm0);
+  v16sf poly_mask = _mm512_castsi512_ps(imm2);
+
+  /* The magic pass: "Extended precision modular arithmetic"
+     x = ((x - y * DP1) - y * DP2) - y * DP3; */
+  xmm1 = *(v16sf*)_ps512_minus_cephes_DP1;
+  xmm2 = *(v16sf*)_ps512_minus_cephes_DP2;
+  xmm3 = *(v16sf*)_ps512_minus_cephes_DP3;
+  xmm1 = _mm512_mul_ps(y, xmm1);
+  xmm2 = _mm512_mul_ps(y, xmm2);
+  xmm3 = _mm512_mul_ps(y, xmm3);
+  x = _mm512_add_ps(x, xmm1);
+  x = _mm512_add_ps(x, xmm2);
+  x = _mm512_add_ps(x, xmm3);
+
+  imm4 = _mm512_sub_epi32(imm4, *(v16si*)_pi32_512_2);
+  imm4 = _mm512_andnot_si512(imm4, *(v16si*)_pi32_512_4);
+  imm4 = _mm512_slli_epi32(imm4, 29);
+
+  v16sf sign_bit_cos = _mm512_castsi512_ps(imm4);
+
+  sign_bit_sin = _mm512_xor_ps(sign_bit_sin, swap_sign_bit_sin);
+
+  /* Evaluate the first polynom  (0 <= x <= Pi/4) */
+  v16sf z = _mm512_mul_ps(x,x);
+  y = *(v16sf*)_ps512_coscof_p0;
+
+  y = _mm512_mul_ps(y, z);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_coscof_p1);
+  y = _mm512_mul_ps(y, z);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_coscof_p2);
+  y = _mm512_mul_ps(y, z);
+  y = _mm512_mul_ps(y, z);
+  v16sf tmp = _mm512_mul_ps(z, *(v16sf*)_ps512_0p5);
+  y = _mm512_sub_ps(y, tmp);
+  y = _mm512_add_ps(y, *(v16sf*)_ps512_1);
+
+  /* Evaluate the second polynom  (Pi/4 <= x <= 0) */
+
+  v16sf y2 = *(v16sf*)_ps512_sincof_p0;
+  y2 = _mm512_mul_ps(y2, z);
+  y2 = _mm512_add_ps(y2, *(v16sf*)_ps512_sincof_p1);
+  y2 = _mm512_mul_ps(y2, z);
+  y2 = _mm512_add_ps(y2, *(v16sf*)_ps512_sincof_p2);
+  y2 = _mm512_mul_ps(y2, z);
+  y2 = _mm512_mul_ps(y2, x);
+  y2 = _mm512_add_ps(y2, x);
+
+  /* select the correct result from the two polynoms */
+  xmm3 = poly_mask;
+  v16sf ysin2 = _mm512_and_ps(xmm3, y2);
+  v16sf ysin1 = _mm512_andnot_ps(xmm3, y);
+  y2 = _mm512_sub_ps(y2,ysin2);
+  y = _mm512_sub_ps(y, ysin1);
+
+  xmm1 = _mm512_add_ps(ysin1,ysin2);
+  xmm2 = _mm512_add_ps(y,y2);
+
+  /* update the sign */
+  *s = _mm512_xor_ps(xmm1, sign_bit_sin);
+  *c = _mm512_xor_ps(xmm2, sign_bit_cos);
+}
+
+
+
+#endif
--- tigergrav/simd.hpp
+++ tigergrav/simd.hpp
@@ -15,6 +15,28 @@
 
 #include <cmath>
 
+#ifdef __AVX512F__
+#define SIMD_FLOAT_LEN 16
+#define _simd_float                 __m512
+#define _simd_int                   __m512i
+#define _mmx_add_ps(a,b)            _mm512_add_ps((a),(b))
+#define _mmx_sub_ps(a,b)            _mm512_sub_ps((a),(b))
+#define _mmx_mul_ps(a,b)            _mm512_mul_ps((a),(b))
+#define _mmx_div_ps(a,b)            _mm512_div_ps((a),(b))
+#define _mmx_sqrt_ps(a)             _mm512_sqrt_ps(a)
+#define _mmx_min_ps(a, b)           _mm512_min_ps((a),(b))
+#define _mmx_max_ps(a, b)           _mm512_max_ps((a),(b))
+#define _mmx_or_ps(a, b)            _mm512_or_ps((a),(b))
+#define _mmx_and_ps(a, b)           _mm512_and_ps((a),(b))
+#define _mmx_andnot_ps(a, b)        _mm512_andnot_ps((a),(b))
+#define _mmx_rsqrt_ps(a)            _mm512_rsqrt_ps(a)
+#define _mmx_add_epi32(a,b)         _mm512_add_epi32((a),(b))
+#define _mmx_sub_epi32(a,b)         _mm512_sub_epi32((a),(b))
+#define _mmx_mul_epi32(a,b)         _mm512_mullo_epi32((a),(b))
+#define _mmx_cvtps_epi32(a)         _mm512_cvtps_epi32((a))
+#define _mmx_fmadd_ps(a,b,c)        _mm512_fmadd_ps ((a),(b),(c))
+#define _mmx_cmp_ps(a,b,c)        	_mm512_cmp_ps_cast(a,b,c)
+#else
 #if defined(__AVX2__)
 #define SIMD_FLOAT_LEN 8
 #define _simd_float                 __m256
@@ -40,6 +62,7 @@
 #error 'Do not have SIMD instructions for this processor'
 #endif
 
+#endif
 class simd_int;
 
 class simd_float {
@@ -167,80 +190,44 @@ public:
 	friend simd_float round(const simd_float);
 
 	friend simd_float exp(const simd_float &a);
-	friend simd_float erfexp(const simd_float &a, simd_float* );
+	friend simd_float erfexp(const simd_float &a, simd_float*);
 
 	friend void sincos(simd_float x, simd_float *s, simd_float *c);
 
-
 	simd_float operator<(simd_float other) const {
-			static const simd_float one(1);
-			static const simd_float zero(0);
-			auto mask = _mmx_cmp_ps(v, other.v, _CMP_LT_OQ);
-			auto rc = _mmx_and_ps(mask, one.v);
-			simd_float v;
-			v.v = rc;
-			return v;
-		}
-	simd_float operator<=(simd_float other) const {
 		static const simd_float one(1);
 		static const simd_float zero(0);
-		auto mask = _mmx_cmp_ps(v, other.v, _CMP_LE_OQ);
-		auto rc = _mmx_and_ps(mask, one.v);
+		auto mask = _mmx_cmp_ps(v, other.v, _CMP_LT_OQ);
 		simd_float v;
-		v.v = rc;
-		return v;
-	}
-	simd_float operator!=(simd_float other) const {
-		static const simd_float one(1);
-		static const simd_float zero(0);
-		auto mask = _mmx_cmp_ps(v, other.v, _CMP_NEQ_OQ);
+#ifdef __AVX512F__
+		auto rc = _mm512_mask_mov(zero.v,mask,one.v);
+#else
 		auto rc = _mmx_and_ps(mask, one.v);
-		simd_float v;
+#endif
 		v.v = rc;
 		return v;
 	}
 
-	simd_float operator==(simd_float other) const {
-		static const simd_float one(1);
-		static const simd_float zero(0);
-		auto mask = _mmx_cmp_ps(v, other.v, _CMP_EQ_OQ);
-		auto rc = _mmx_and_ps(mask, one.v);
-		simd_float v;
-		v.v = rc;
-		return v;
-	}
 	simd_float operator>(simd_float other) const {
 		static const simd_float one(1);
 		static const simd_float zero(0);
 		auto mask = _mmx_cmp_ps(v, other.v, _CMP_GT_OQ);
-		auto rc = _mmx_and_ps(mask, one.v);
 		simd_float v;
-		v.v = rc;
+#ifdef __AVX512F__
+		auto rc = _mm512_mask_mov(zero.v,mask,one.v);
+#else
+		auto rc = _mmx_and_ps(mask, one.v);
+#endif
 		return v;
 	}
-
-	simd_float operator>=(simd_float other) const {
-		static const simd_float one(1);
-		static const simd_float zero(0);
-		auto mask = _mmx_cmp_ps(v, other.v, _CMP_GE_OQ);
-		auto rc = _mmx_and_ps(mask, one.v);
-		simd_float v;
-		v.v = rc;
-		return v;
-	}
-
-
-
 
 };
 
-
 inline simd_float round(const simd_float a) {
 	simd_float v;
-	v.v = _mm256_round_ps(a.v,_MM_FROUND_TO_NEAREST_INT);
+	v.v = _mm256_round_ps(a.v, _MM_FROUND_TO_NEAREST_INT);
 	return v;
 }
-
 
 inline simd_float sin(simd_float x) {
 	// From : http://mooooo.ooo/chebyshev-sine-approximation/
@@ -265,9 +252,8 @@ inline simd_float sin(simd_float x) {
 }
 
 inline simd_float cos(simd_float x) {
-	return sin(x + simd_float(M_PI/2.0));
+	return sin(x + simd_float(M_PI / 2.0));
 }
-
 
 inline void sincos(simd_float x, simd_float *s, simd_float *c) {
 //	sincos256_ps(x.v, &(s->v), &(c->v));
@@ -279,11 +265,15 @@ inline void sincos(simd_float x, simd_float *s, simd_float *c) {
 
 inline simd_float exp(const simd_float &a) {
 	simd_float v;
+#ifdef __AVX512F__
+	v.v = exp512_ps(a.v);
+#else
 	v.v = exp256_ps(a.v);
+#endif
 	return v;
 }
 
-inline simd_float erfexp(const simd_float &x, simd_float* e) {
+inline simd_float erfexp(const simd_float &x, simd_float *e) {
 	simd_float v;
 	const simd_float p(0.3275911);
 	const simd_float a1(0.254829592);
@@ -300,7 +290,6 @@ inline simd_float erfexp(const simd_float &x, simd_float* e) {
 	return simd_float(1) - (a1 * t1 + a2 * t2 + a3 * t3 + a4 * t4 + a5 * t5) * *e;
 	return v;
 }
-
 
 inline simd_float fma(const simd_float &a, const simd_float &b, const simd_float &c) {
 	simd_float v;
