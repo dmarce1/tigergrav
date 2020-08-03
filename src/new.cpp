2ff39e5689e8f3215306f53e08e4c62321360223
Dominic Marcello <dmarce504@gmail.com>
2020-08-03 11:34:21 -0500

    Using tcmalloc

--- src/new.cpp
+++ src/new.cpp
@@ -4,25 +4,25 @@
  *  Created on: Jul 18, 2020
  *      Author: dmarce1
  */
-#ifdef __AVX512F__
-#define ALIGN 64
-#else
-#define ALIGN 32
-#endif
-
-#include <cstdlib>
-#include <cstdio>
-
-void* operator new(std::size_t sz) {
-	void *ptr;
-	if ( posix_memalign(&ptr,ALIGN,sz) != 0) {
-		printf("Memory allcation error\n");
-		abort();
-	}
-	return ptr;
-
-}
-
-void operator delete(void* ptr) {
-	free(ptr);
-}
+//#ifdef __AVX512F__
+//#define ALIGN 64
+//#else
+//#define ALIGN 32
+//#endif
+//
+//#include <cstdlib>
+//#include <cstdio>
+//
+//void* operator new(std::size_t sz) {
+//	void *ptr;
+//	if ( posix_memalign(&ptr,ALIGN,sz) != 0) {
+//		printf("Memory allcation error\n");
+//		abort();
+//	}
+//	return ptr;
+//
+//}
+//
+//void operator delete(void* ptr) {
+//	free(ptr);
+//}
